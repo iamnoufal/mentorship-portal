84d146a91e84bb7d7ac1d9b538818eacd0985403
iamnoufal <jnrahman12@gmail.com>
2023-12-06 11:35:59 +0530

    select available mentors fix

--- src/pages/api/mentor/available.ts
+++ src/pages/api/mentor/available.ts
@@ -7,7 +7,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Array<UserType> | unknown>
 ) {
-  let data =
-    await prisma.$queryRaw`select regno, name, email, department, image, year from "User" where email not in (select "mentorID" from "User" where "mentorID" is not null group by "mentorID" having count("mentorID")>=3) and (type like 'admin' or type like 'mentor')`;
+  let data: any =
+    await prisma.$queryRaw`select regno, name, email, department, image, year, skills, hobbies from "User" where email not in (select "mentorID" from "User" where "mentorID" is not null group by "mentorID" having count("mentorID")>=3) and (type like 'admin' or type like 'mentor')`;
+  for (let i of data) {
+    i!.skills = i?.skills
+      ? (i?.skills?.split(", ") as unknown as string)
+      : ([] as unknown as string);
+    i!.hobbies = i!.hobbies
+      ? (i!.hobbies!.split(", ") as unknown as string)
+      : ([] as unknown as string);
+  }
   return res.status(200).json(data);
 }
